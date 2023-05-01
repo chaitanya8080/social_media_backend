ddadc816ddec7acad7a6d67d5f508a4638d3c07e
chaitanya8080 <girase.chaitanya@gmail.com>
2023-05-01 14:16:18 +0530

    user Model added

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const userRoutes = require('./Routes/userRoutes')
+const booksRoutes = require('./Routes/booksRoutes.js')
+const dotenv =  require("dotenv");
+const  connection  =  require("./db/db");
+dotenv.config()
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', userRoutes);
+
+
+app.listen(7004, () => {
+  try {
+    connection
+    console.log("Server started on port 7004");
+  } catch (error) {
+    console.log(error.message)
+  }
+});
+
+
---
+++ models/userModel.js
@@ -0,0 +1,63 @@
+
+
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  dob: {
+    type: Date,
+  },
+  bio: {
+    type: String
+  },
+  posts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+  friends: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  friendRequests: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+});
+
+
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+      return next();
+    }    
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  });
+  
+  UserSchema.methods.getJWTToken = function () {
+    return Jwt.sign({ _id: this._id }, process.env.JWT_SECRETKEY, {
+      expiresIn: "15d",
+    });
+  };
+  
+  UserSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
+
+  const User = mongoose.model('User', UserSchema);
+
+  module.exports = User;
